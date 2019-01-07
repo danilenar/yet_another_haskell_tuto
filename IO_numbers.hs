73954eb943f6ab0f6d072a09bbe13b527ef497a1
castor <daniellenarduzzi@gmail.com>
2019-01-06 21:06:04 -0300

    TRIPLE DATA TYPE Write a data type declaration for Triple, a type which contains three elements, all of different types. Write functions tripleFst, tripleSnd and tripleThr to extract respectively the first, second and third.

--- IO_numbers.hs
+++ IO_numbers.hs
@@ -6,17 +6,14 @@ import System.IO
 main = do
     hSetBuffering stdin LineBuffering
     list <- inputIntList
-    let sum = foldl (+) 0 list
-    let mult = foldl (*) 1 list
-    putStrLn ("The sum is "++show(sum))
-    putStrLn ("The product is "++show(mult))
-    --putStrLn(show(head list)++" factorial is "++show(factorial(head list)))
+    putStrLn ("The sum is "++show(sum list))
+    putStrLn ("The product is "++show(product list))
     printFactorial list
 
 inputIntList = do
     putStrLn "Enter a number (0 to stop):"
     input <- getLine
-    let intValue = read input
+    let intValue = read input :: Int
     if intValue == 0
         then return []
         else do
---
+++ triple_type_basics.hs
@@ -0,0 +1,5 @@
+data Triple a b c = Triple a b c
+
+tripleFst (Triple a b c) = a
+tripleSnd (Triple a b c) = b
+tripleThr (Triple a b c) = c
